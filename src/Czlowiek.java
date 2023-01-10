fd8f78c33e925ccbb637bb4d036c059e3b615068
kamil1808 <122380315+kamil1808@users.noreply.github.com>
2023-01-10 21:51:05 +0100

    klasa bazowa + nowa metoda wersja 1.3

---
+++ src/Czlowiek.java
@@ -0,0 +1,5 @@
+public class Czlowiek  {
+    String imie;
+    String nazwisko;
+
+}
--- src/Main.java
+++ src/Main.java
@@ -7,63 +7,77 @@ public class Main {
 
         //klasa składa się z 9 uczniów
         // a określa ilu uczniów chcesz wyświetlić (max 9)
-        utworzliste(22);
-        //wypiszliste();
+        utworzliste1(9);
+        utworzliste2(9);
+        wypiszliste(utworzliste1(9), utworzliste2(9));
+
     }
 
-    public static void utworzliste(int a) {
+    public static String[] utworzliste1(int a) {
         if (a > 9) {
             a = 9;
         }
         String[] imiona = new String[9];
-        String[] nazwiska = new String[9];
+
         for (int y = 0; y < a; y++) {
             imiona[y] = new String();
-            nazwiska[y] = new String();
+
             imiona[0] = "Jan";
-            nazwiska[0] = "Kotry";
-
             imiona[1] = "Kuba";
-            nazwiska[1] = "Owalny";
-
             imiona[2] = "Ola";
-            nazwiska[2] = "Miskowiec";
-
             imiona[3] = "Ania";
-            nazwiska[3] = "Gil";
-
             imiona[4] = "Adam";
-            nazwiska[4] = "Zurek";
-
             imiona[5] = "Olek";
-            nazwiska[5] = "Taton";
-
             imiona[6] = "Iza";
-            nazwiska[6] = "Lis";
-
             imiona[7] = "Marek";
-            nazwiska[7] = "Siwor";
-
             imiona[8] = "Ewa";
+        }
+        return imiona;
+    }
+    public static String[] utworzliste2(int a) {
+        if (a > 9) {
+            a = 9;
+        }
+        String[] nazwiska = new String[9];
+        for (int y = 0; y < a; y++) {
+            nazwiska[y] = new String();
+
+            nazwiska[0] = "Kotry";
+            nazwiska[1] = "Owalny";
+            nazwiska[2] = "Miskowiec";
+            nazwiska[3] = "Gil";
+            nazwiska[4] = "Zurek";
+            nazwiska[5] = "Taton";
+            nazwiska[6] = "Lis";
+            nazwiska[7] = "Siwor";
             nazwiska[8] = "Adamowicz";
         }
+        return nazwiska;
+    }
 
-        //wypiszliste();
 
-        Uczen[] uczen = new Uczen[a];
-        for (int i = 0; i < a; i++) {
-            uczen[i] = new Uczen();
-            uczen[i].imie = imiona[i];
-            uczen[i].nazwisko = nazwiska[i];
-            //uczen[i].oceny = ocenyyy[i];
-            uczen[i].numer = i + 1;
-            if (uczen[i].imie == "Ania" || uczen[i].imie == "Ewa") {
-                uczen[i].UczSie();
-            }
 
+
+        public static void wypiszliste(String [] imiona, String [] nazwiska) {
+        
+            Uczen[] uczen = new Uczen[9];
+            for (int i = 0; i < 9; i++) {
+                uczen[i] = new Uczen();
+                uczen[i].imie = imiona[i];
+                uczen[i].nazwisko = nazwiska[i];
+
+                //uczen[i].oceny = ocenyyy[i];
+
+                uczen[i].numer = i + 1;
+                if (uczen[i].imie == "Ania" || uczen[i].imie == "Ewa") {
+                    uczen[i].UczSie();
+                }
             System.out.println("nr." + uczen[i].numer + " " + uczen[i].imie + " " + uczen[i].nazwisko + " Uczy sie:" + uczen[i].CzySieUczy);
         }
+            }
+
+
+
     }
-}
 
 
