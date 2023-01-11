0fac7675c3b527c0820c6e1036cd82246c47da71
kamil1808 <122380315+kamil1808@users.noreply.github.com>
2023-01-11 16:05:33 +0100

    wersja 1.4
    Już tylko 1 metoda zwraca wynik, i to tablice obiektów Uczen zamiast składowych ucznia

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,25 @@ public class Main {
         System.out.println("Witaj w Librus 3C");
         System.out.println(" ");
 
-        //klasa składa się z 9 uczniów
+        // klasa składa się z 9 uczniów
         // a określa ilu uczniów chcesz wyświetlić (max 9)
-        utworzliste1(9);
-        utworzliste2(9);
-        wypiszliste(utworzliste1(9), utworzliste2(9));
+        //utworzliste(9);
+        //Arraylist<uczen> uczen[] lista1 =
+
+        wypiszliste(utworzliste(9));
     }
 
-    public static String[] utworzliste1(int a) {
+    public static Uczen[] utworzliste(int a) {
         if (a > 9) {
             a = 9;
         }
         String[] imiona = new String[9];
-
+        String[] nazwiska = new String[9];
+        Uczen[] uczen = new Uczen[9];
         for (int y = 0; y < a; y++) {
             imiona[y] = new String();
-
+            nazwiska[y] = new String();
+            uczen[y] = new Uczen();
             imiona[0] = "Jan";
             imiona[1] = "Kuba";
             imiona[2] = "Ola";
@@ -30,17 +33,6 @@ public class Main {
             imiona[6] = "Iza";
             imiona[7] = "Marek";
             imiona[8] = "Ewa";
-        }
-        return imiona;
-    }
-    public static String[] utworzliste2(int a) {
-        if (a > 9) {
-            a = 9;
-        }
-        String[] nazwiska = new String[9];
-        for (int y = 0; y < a; y++) {
-            nazwiska[y] = new String();
-
             nazwiska[0] = "Kotry";
             nazwiska[1] = "Owalny";
             nazwiska[2] = "Miskowiec";
@@ -50,25 +42,23 @@ public class Main {
             nazwiska[6] = "Lis";
             nazwiska[7] = "Siwor";
             nazwiska[8] = "Adamowicz";
+
+                uczen[y].imie = imiona[y];
+                uczen[y].nazwisko = nazwiska[y];
+
+                //uczen[i].oceny = ocenyyy[i];
+                uczen[y].numer = y + 1;
+                if (uczen[y].imie == "Ania" || uczen[y].imie == "Ewa") {
+                    uczen[y].UczSie();
+                }
         }
-        return nazwiska;
+        return uczen;
     }
 
 
-        public static void wypiszliste(String [] imiona, String [] nazwiska) {
-        
-            Uczen[] uczen = new Uczen[9];
+        public static void wypiszliste(Uczen [] uczen) {
+
             for (int i = 0; i < 9; i++) {
-                uczen[i] = new Uczen();
-                uczen[i].imie = imiona[i];
-                uczen[i].nazwisko = nazwiska[i];
-
-                //uczen[i].oceny = ocenyyy[i];
-
-                uczen[i].numer = i + 1;
-                if (uczen[i].imie == "Ania" || uczen[i].imie == "Ewa") {
-                    uczen[i].UczSie();
-                }
             System.out.println("nr." + uczen[i].numer + " " + uczen[i].imie + " " + uczen[i].nazwisko + " Uczy sie:" + uczen[i].CzySieUczy);
         }
             }
